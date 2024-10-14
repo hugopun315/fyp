cab679ef572a6c5e9495de134ece440c936b1814
hugop <23239298@life.hkbu.edu.hk>
2024-10-14 23:44:25 +0800

    14/10/24 loginFunction

--- app/src/main/java/com/example/fyp/createAccount.kt
+++ app/src/main/java/com/example/fyp/createAccount.kt
@@ -1,17 +1,22 @@
 package com.example.fyp
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class createAccount : AppCompatActivity() {
 
-
+    private lateinit var  auth : FirebaseAuth
 
     private lateinit var hightlEditText: EditText
     private lateinit var weightEditText: EditText
@@ -21,11 +26,19 @@ class createAccount : AppCompatActivity() {
 
     private lateinit var emailEditText: EditText
     private lateinit var passwordEditText: EditText
+    private lateinit var sexRadio: RadioGroup
+    private lateinit var habitRadio: RadioGroup
+    private lateinit var targetRadio: RadioGroup
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
-
+        auth = FirebaseAuth.getInstance()
+       sexRadio = findViewById(R.id.sex_list)
+        var selectedSex: String? = null
+        var selectedTarget: String? = null
+        var selectedHabit: String? = null
         emailEditText = findViewById(R.id.inputEmail)
         passwordEditText = findViewById(R.id.inputPassword)
         hightlEditText = findViewById(R.id.inputHight)
@@ -35,9 +48,73 @@ class createAccount : AppCompatActivity() {
 
         createButton = findViewById(R.id.btnCreate)
 
+
+/*
+        sexRadio.setOnCheckedChangeListener { group, checkedId ->
+            selectedSex = when (checkedId) {
+                R.id.radio_male -> "Male"
+                R.id.radio_female -> "Female"
+                else -> null
+            }
+        }
+
+        habitRadio.setOnCheckedChangeListener { group, checkedId ->
+            selectedHabit = when (checkedId) {
+                R.id.noExercise -> "No Exercise"
+                R.id.lightExercise -> "Light Exercise"
+                R.id.middleExercise -> "Middle Exercise"
+                R.id.heavyExercise -> "Heavy Exercise"
+                R.id.veryheavyExercise -> "Very Heavy Exercise"
+                else -> null
+            }
+        }
+
+        targetRadio.setOnCheckedChangeListener { group, checkedId ->
+            selectedTarget = when (checkedId) {
+                R.id.LoseWeight -> "Lose Weight"
+                R.id.GainWeight -> "Gain Weight"
+                R.id.MaintainWeight -> "Maintain Weight"
+                else -> null
+            }
+        }
+
+        */
+
         createButton.setOnClickListener {
+            /*
             val intent = Intent(this,loginActivity::class.java)
             startActivity(intent)
+*/
+
+            var  email = emailEditText.text.toString()
+            var password = passwordEditText.text.toString()
+            var hight = hightlEditText.text.toString()
+            var weight = weightEditText.text.toString()
+            var age = ageEditText.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener() { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            baseContext,
+                            "Account created.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+
+                        val intent = Intent(this,homeActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+
+                    }
+                }
+
 
         }
 
--- app/src/main/java/com/example/fyp/loginActivity.kt
+++ app/src/main/java/com/example/fyp/loginActivity.kt
@@ -1,14 +1,18 @@
 package com.example.fyp
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class loginActivity : AppCompatActivity() {
 
@@ -17,11 +21,25 @@ class loginActivity : AppCompatActivity() {
     private lateinit var loginButton: Button
     private lateinit var createAccountTextView: TextView
     private lateinit var errorText: EditText
+    private lateinit var  auth : FirebaseAuth
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intent = Intent(this,homeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-
+        auth = FirebaseAuth.getInstance()
         emailEditText = findViewById(R.id.inputEmail)
         passwordEditText = findViewById(R.id.inputPassword)
         loginButton = findViewById(R.id.btnLogin)
@@ -39,13 +57,40 @@ class loginActivity : AppCompatActivity() {
 
         loginButton = findViewById(R.id.btnLogin)
         loginButton.setOnClickListener {
-            val intent = Intent(this,homeActivity::class.java)
-            startActivity(intent)
+            var email = emailEditText.text.toString()
+            var password = passwordEditText.text.toString()
+
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener() { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            baseContext,
+                            "Login Success",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        val intent = Intent(this,homeActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+
+                    }
+                }
+
+
+
+
         }
 
         createAccountTextView.setOnClickListener{
             val intent = Intent(this,createAccount::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
